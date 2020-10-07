08c31fd8e9fae7e4d9e7e0f2514bd87c64ae3e3e
Joshua Lovelace <rakmial@gmail.com>
2020-10-07 15:58:24 -0700

    updates class specs for shoulda

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 describe Album do
-  it "has many songs" do
-    album = Album.create name: "album"
-    song1 = Song.create name: "song1", album_id: album.id
-    song2 = Song.create name: "song2", album_id: album.id
-    expect(album.songs).to eq [song1, song2]
-  end
+  it { should have_many :songs }
 end
